46af2c621a4aec7297ed04df9bba9b8c91611109
zhangjie <zhangjie@xianlai-inc.com>
2019-07-02 11:33:37 +0800

    feat:数组的题目

--- app/src/test/java/lanchong/iloveu/algorithm/ArrayUnitTest.java
+++ app/src/test/java/lanchong/iloveu/algorithm/ArrayUnitTest.java
@@ -35,6 +35,26 @@ public class ArrayUnitTest {
         System.out.print("result:" + new Array().isValidSudoku(sudoku));
     }
 
+    @Test
+    public void threeSum() {
+//        int[] arr = {-1, 0, 1, 2, -1, -4};
+//        int[] arr = {0,0,0,0};
+//        int[] arr = {0,0,0};
+        int[] arr = {-2,0,0,2,2};
+//        int[] arr = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+//        int[] arr = {3,0,-2,-1,1,2};
+//        int[] arr = {-2,-1,0,1,2,3};
+//        int[] arr = {-2,-1,0,2};
+        System.out.print("result:" + new Array().threeSum(arr));
+    }
+
+    @Test
+    public void majorityElement() {
+//        int[] arr = {2,0,0,2,2};
+        int[] arr = {3,2,3};
+//        int[] arr = {2,2,1,1,1,2,2};
+        System.out.print("result:" + new Array().majorityElement(arr));
+    }
 
 
 
